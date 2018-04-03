eb61f003875b781d384b5965223b93a24ad34ac8
Jason <jayman24678@gmail.com>
2018-04-03 13:43:29 -0400

    removed unused imports in game class

--- core/src/com/yetea/lgdxgame/MyGdxGame.java
+++ core/src/com/yetea/lgdxgame/MyGdxGame.java
@@ -1,11 +1,7 @@
 package com.yetea.lgdxgame;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.yetea.lgdxgame.Screens.TitleScreen;
 
